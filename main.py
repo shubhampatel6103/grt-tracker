23d6c0e6c619e6593697ac3048d14e01fc375d52
shubhampatel6103 <shubhampatel6103@gmail.com>
2025-05-21 23:34:22 -0400

    try something

--- main.py
+++ main.py
@@ -49,9 +49,12 @@ def scrape_grt_stop(stop_number):
         print("Configuring Chrome options")
         # Configure Chrome options
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-software-rasterizer")
+        chrome_options.add_argument("--single-process")
         chrome_options.binary_location = "/opt/chrome/chrome"
         print("Installing Chrome driver")
         service = Service("/usr/bin/chromedriver")
